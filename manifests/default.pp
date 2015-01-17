a076611cbca39e5348e188f0719b897bbdef6948
Nils Domrose <nils.domrose@inovex.de>
2015-01-17 22:57:06 +0100

    added agent class

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,4 +6,8 @@ node default {
   class {'puppet::master':
     require => Class['puppet::repo::puppetlabs']
   }
+  class {'puppet::agent':
+    require => Class['puppet::repo::puppetlabs']
+  }
+
 }
