fb14330ccb4f9e8c30d4d120c931c67d690554ab
Nils Domrose <nils.domrose@inovex.de>
2015-01-17 23:56:41 +0100

    added ssh config

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,14 +1,22 @@
 node default {
   include ssh
   include puppet::repo::puppetlabs
+  class {'puppet::agent':
+    require => Class['puppet::repo::puppetlabs']
+  }
+
+}
+
+node /^lxcpuppet\.*/ {
+  include ssh
+  include puppet::repo::puppetlabs
+  class {'puppet::agent':
+    require => Class['puppet::repo::puppetlabs']
+  }
   class {'puppetdb':
     require => Class['puppet::repo::puppetlabs']
   }
   class {'puppet::master':
     require => Class['puppet::repo::puppetlabs']
   }
-  class {'puppet::agent':
-    require => Class['puppet::repo::puppetlabs']
-  }
-
 }
